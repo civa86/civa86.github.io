072ad975a2d314eee9af7cfd69f36a80f4f3dae2
Dario Civallero <dario.civallero@etiqa.it>
2017-04-28 12:47:09 +0200

    building

--- app/src/routes.js
+++ app/src/routes.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import { Route, Redirect } from 'react-router'
+import { Route } from 'react-router'
 
 import App from './containers/App';
 
 export default (
     <Route>
         <Route path="/" component={App}/>
-        <Redirect from="*" to="index"/>
     </Route>
 );
