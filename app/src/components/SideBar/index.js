88914bbfded349775d91a89bb3d917e6d3b07527
Dario Civallero <dario.civallero@gmail.com>
2017-04-30 15:04:43 +0200

    sidebar

--- app/src/components/SideBar/index.js
+++ app/src/components/SideBar/index.js
@@ -8,6 +8,17 @@ class SideBar extends Component {
                     <div className="title">
                         <i className="octicon octicon-repo"/> Dario Civallero
                     </div>
+                    <ul>
+                        <li className="directory selected">
+                            <i className="chevron octicon octicon-chevron-right"/>
+                            <i className="type octicon octicon-file-directory"/>
+                            <span className="list-label">asd</span>
+                        </li>
+                        <li className="file">
+                            <i className="type octicon octicon-file"/>
+                            <span className="list-label">asd</span>
+                        </li>
+                    </ul>
                 </div>
             </div>
         );
