bcb5bd3acc5ad340dbc4fc94f3ab570bb626fc27
Dario Civallero <dario.civallero@gmail.com>
2017-04-30 14:36:04 +0200

    footer

--- app/src/containers/App.js
+++ app/src/containers/App.js
@@ -5,6 +5,7 @@ import * as appAction from '../actions/AppActions';
 import * as editorAction from '../actions/EditorActions';
 
 //Components
+import Footer from '../components/Footer';
 import SplitPane from 'react-split-pane';
 import SideBar from '../components/SideBar';
 import Tabs, { Tab } from '../components/Tabs';
@@ -37,79 +38,84 @@ class App extends Component {
     render () {
         const { dispatch, editor } = this.props;
         return (
-            <SplitPane split="vertical" minSize={105} defaultSize={205}>
-                <SideBar/>
-                <div className="content">
-                    <div className={this.buildEmptyClass('tab-bar')}>
-                        <Tabs active={editor.activeTab}
-                              onTabSwitch={tab => dispatch(editorAction.tabSelect(tab))}
-                              onTabClose={tab => dispatch(editorAction.tabClose(tab))}
-                              onTabPositionChange={(a, b) => dispatch(editorAction.tabChangePosition(a, b))}
-                              draggable={true}>
-                            {this.renderTabs()}
-                        </Tabs>
-                    </div>
-                    <div className={this.buildEmptyClass('container-fluid content-editor')}>
-                        <div className="row">
-                            <div className="col-xs-12">
-                                {
-                                    this.getOpenedTabs().length === 0 &&
-                                    <Reload onReload={() => dispatch(appAction.reload())}/>
-                                }
-                                {
-                                    this.getOpenedTabs().length > 0 &&
-                                    <Code>
-                                        <span>def print_hi(name)</span>
-                                        <span>	puts "Hi, #{name}"</span>
-                                        <span>end</span>
-                                        <span></span>
-                                        <span>print_hi('Tom')</span>
-                                        <span>def print_hi(name)</span>
-                                        <span>	puts "Hi, #{name}"</span>
-                                        <span>end</span>
-                                        <span></span>
-                                        <span>print_hi('Tom')</span>
-                                        <span>#=> prints 'Hi, Tom' to STDOUT.</span>
-                                        <span>def print_hi(name)</span>
-                                        <span>	puts "Hi, #{name}"</span>
-                                        <span>end</span>
-                                        <span></span>
-                                        <span>print_hi('Tom')</span>
-                                        <span>def print_hi(name)</span>
-                                        <span>	puts "Hi, #{name}"</span>
-                                        <span>end</span>
-                                        <span></span>
-                                        <span>print_hi('Tom')</span>
-                                        <span>#=> prints 'Hi, Tom' to STDOUT.</span>
-                                        <span>def print_hi(name)</span>
-                                        <span>	puts "Hi, #{name}"</span>
-                                        <span>end</span>
-                                        <span></span>
-                                        <span>print_hi('Tom')</span>
-                                        <span>def print_hi(name)</span>
-                                        <span>	puts "Hi, #{name}"</span>
-                                        <span>end</span>
-                                        <span></span>
-                                        <span>print_hi('Tom')</span>
-                                        <span>#=> prints 'Hi, Tom' to STDOUT.</span>
-                                        <span>def print_hi(name)</span>
-                                        <span>	puts "Hi, #{name}"</span>
-                                        <span>end</span>
-                                        <span></span>
-                                        <span>print_hi('Tom')</span>
-                                        <span>def print_hi(name)</span>
-                                        <span>	puts "Hi, #{name}"</span>
-                                        <span>end</span>
-                                        <span></span>
-                                        <span>print_hi('Tom')</span>
-                                        <span>#=> prints 'Hi, Tom' to STDOUT.</span>
-                                    </Code>
-                                }
+            <div>
+                <div className="panels">
+                    <SplitPane split="vertical" minSize={105} defaultSize={205}>
+                        <SideBar/>
+                        <div className="content">
+                            <div className={this.buildEmptyClass('tab-bar')}>
+                                <Tabs active={editor.activeTab}
+                                      onTabSwitch={tab => dispatch(editorAction.tabSelect(tab))}
+                                      onTabClose={tab => dispatch(editorAction.tabClose(tab))}
+                                      onTabPositionChange={(a, b) => dispatch(editorAction.tabChangePosition(a, b))}
+                                      draggable={true}>
+                                    {this.renderTabs()}
+                                </Tabs>
+                            </div>
+                            <div className={this.buildEmptyClass('container-fluid content-editor')}>
+                                <div className="row">
+                                    <div className="col-xs-12">
+                                        {
+                                            this.getOpenedTabs().length === 0 &&
+                                            <Reload onReload={() => dispatch(appAction.reload())}/>
+                                        }
+                                        {
+                                            this.getOpenedTabs().length > 0 &&
+                                            <Code>
+                                                <span>def print_hi(name)</span>
+                                                <span>	puts "Hi, #{name}"</span>
+                                                <span>end</span>
+                                                <span></span>
+                                                <span>print_hi('Tom')</span>
+                                                <span>def print_hi(name)</span>
+                                                <span>	puts "Hi, #{name}"</span>
+                                                <span>end</span>
+                                                <span></span>
+                                                <span>print_hi('Tom')</span>
+                                                <span>#=> prints 'Hi, Tom' to STDOUT.</span>
+                                                <span>def print_hi(name)</span>
+                                                <span>	puts "Hi, #{name}"</span>
+                                                <span>end</span>
+                                                <span></span>
+                                                <span>print_hi('Tom')</span>
+                                                <span>def print_hi(name)</span>
+                                                <span>	puts "Hi, #{name}"</span>
+                                                <span>end</span>
+                                                <span></span>
+                                                <span>print_hi('Tom')</span>
+                                                <span>#=> prints 'Hi, Tom' to STDOUT.</span>
+                                                <span>def print_hi(name)</span>
+                                                <span>	puts "Hi, #{name}"</span>
+                                                <span>end</span>
+                                                <span></span>
+                                                <span>print_hi('Tom')</span>
+                                                <span>def print_hi(name)</span>
+                                                <span>	puts "Hi, #{name}"</span>
+                                                <span>end</span>
+                                                <span></span>
+                                                <span>print_hi('Tom')</span>
+                                                <span>#=> prints 'Hi, Tom' to STDOUT.</span>
+                                                <span>def print_hi(name)</span>
+                                                <span>	puts "Hi, #{name}"</span>
+                                                <span>end</span>
+                                                <span></span>
+                                                <span>print_hi('Tom')</span>
+                                                <span>def print_hi(name)</span>
+                                                <span>	puts "Hi, #{name}"</span>
+                                                <span>end</span>
+                                                <span></span>
+                                                <span>print_hi('Tom')</span>
+                                                <span>#=> prints 'Hi, Tom' to STDOUT.</span>
+                                            </Code>
+                                        }
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </SplitPane>
                 </div>
-            </SplitPane>
+                <Footer/>
+            </div>
         );
     }
 }
@@ -121,3 +127,4 @@ function mapStateToProps (state) {
 }
 
 export default connect(mapStateToProps)(App);
+
