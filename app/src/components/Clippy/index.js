be74ac37a2712b0fa3f32462a33d72bd05af9e92
Dario Civallero <dario.civallero@gmail.com>
2017-05-10 21:29:55 +0200

    copy 2 clipboard

--- app/src/components/Clippy/index.js
+++ app/src/components/Clippy/index.js
@@ -3,6 +3,32 @@ import PropTypes from 'prop-types';
 import $ from 'jquery';
 
 class Clippy extends Component {
+
+    constructor (props) {
+        super(props);
+
+        this.enabledFeature = false;
+
+        if (document && document.execCommand && typeof document.execCommand === 'function') {
+            this.enabledFeature = true;
+        }
+    }
+
+    add2clipboard (text) {
+        const tmpEl = document.createElement("textarea");
+
+        tmpEl.style.opacity = 0;
+        tmpEl.style.position = "absolute";
+        tmpEl.style.pointerEvents = "none";
+        tmpEl.style.zIndex = -1;
+        tmpEl.value = text;
+
+        document.body.appendChild(tmpEl);
+        tmpEl.select();
+        document.execCommand("copy");
+        document.body.removeChild(tmpEl);
+    }
+
     onCopyClick () {
         const clone = $('.code').clone();
 
@@ -16,19 +42,26 @@ class Clippy extends Component {
                                 .replace(/return/g, '\nreturn')
                                 .replace(/newWin\./g, '\nnewWin\.');
 
-        return textToCopy; //TODO copy in clipboard
-
+        this.add2clipboard(textToCopy);
     }
 
     render () {
         const { label } = this.props;
 
         return (
-            <span className="cursor-pointer"
-                  onClick={() => this.onCopyClick()}
-            >
-               <i className="octicon octicon-clippy"/> {label}
-           </span>
+            <span>
+                {
+                    this.enabledFeature &&
+                    <span className="cursor-pointer"
+                          onClick={() => this.onCopyClick()}>
+                        <i className="octicon octicon-clippy"/> {label}
+                    </span>
+                }
+                {
+                    !this.enabledFeature &&
+                    label
+                }
+            </span>
         );
     }
 }
