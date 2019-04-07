b74cf3a3f2a6d6d79152fa5bcbb71b560af9acee
Dario Civallero <dario.civallero@gmail.com>
2019-04-07 22:11:05 +0200

    contents

---
+++ src/components/Contents/Bio.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// Style
+// import './style.scss'
+
+const Bio = props => {
+  //   const { content } = props
+
+  return <div>BIO</div>
+}
+
+export default Bio
---
+++ src/components/Contents/Skills.js
@@ -0,0 +1,11 @@
+import React from 'react'
+// Style
+// import './style.scss'
+
+const Skills = props => {
+  //   const { content } = props
+
+  return <div>SKILLS</div>
+}
+
+export default Skills
