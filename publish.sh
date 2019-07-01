344afade4b4c57a3b533c9e2200098924f7b44f8
Dario Civallero <dario.civallero@gmail.com>
2019-07-01 15:39:45 +0200

    update pipeline

--- publish.sh
+++ publish.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-GH_REF="`node -p -e "require('./package.json').repository.url.replace('https://', '').trim()"`"
 PACKAGE_VERSION=`node -p -e "require('./package.json').version"`
+PUBLISH_TIME="`date +'%Y-%m-%d %H:%M:%S'`"
 
-echo "/***** APPLICATION DISTRIBUTION ****/"
-npm run build || { echo 'APPLICATION DISTRIBUTION: failed'; exit 1; }
-
-echo "/***** ENTER APPLICATION DIST DIRECTORY ****/"
 cd build
 
 echo "/***** VERSION ****/"
@@ -14,22 +10,3 @@ echo "VERSION $PACKAGE_VERSION" > ./version.txt
 echo "COMMIT $TRAVIS_COMMIT" >> ./version.txt
 echo "BUILD BRANCH $TRAVIS_BRANCH" >> ./version.txt
 echo "BUILD DATE $PUBLISH_TIME" >> ./version.txt
-
-echo "/***** CREATE TMP REPO TO PUBLISH ****/"
-git init
-
-git config user.name "Publisher Bot"
-git config user.email "dario.civallero@gmail.com"
-git add .
-
-echo "/***** COMMIT VERSION ****/"
-PUBLISH_TIME="`date +'%Y-%m-%d %H:%M:%S'`"
-git commit -m "Publish version $PACKAGE_VERSION on $PUBLISH_TIME"
-
-echo "/***** PUSHING $GH_REF ****/"
-git push --force --quiet "https://${GITHUB_TOKEN}@$GH_REF" master:master > /dev/null 2>&1
-
-echo "/***** CLEAN TMP REPO ****/"
-rm -rf .git
-
-echo "/***** FINISH ****/"
